516e640408ded0166a1e0cb9282d4efeb157a585
Soulin Maikadua <sou.dev3@gmail.com>
2024-06-04 23:16:14 +0700

    home commit

---
+++ components/WordComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="bg-blue-100 p-4 flex items-center">
+            <div>
+                <IconSpeakerWave />
+            </div>
+            <div class="ml-4">
+                <h1 class="text-xl font-bold">
+                    {{ props.item.korean }} {{ props.item.read
+                    }}<span class="text-red-500 text-sm ml-2"
+                        >({{ props.item.speech }})</span
+                    >
+                </h1>
+                <p>{{ props.item.lao }}</p>
+                <p>{{ props.item.english }}</p>
+            </div>
+        </div>
+        <div v-for="(example, index) in props.item.examples" :key="index" class="bg-white p-4">
+            <div class="flex items-center">
+                <div>
+                    <IconSpeakerWave />
+                </div>
+                <div class="ml-4">
+                    <p>{{example.korean}}</p>
+                    <p>{{example.read}}</p>
+                </div>
+            </div>
+            <div class="flex items-center">
+                <div>
+                    <IconSpeakerWave />
+                </div>
+                <div class="ml-4">
+                    <p>{{ example.english }}</p>
+                    <p>{{example.lao}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    item: {
+        type: Object,
+        default: () => {},
+    },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,68 @@
 <template>
-     <main class="max-w-screen-xl mx-auto px-4 min-h-screen">
-        <div>
-            hhh
+    <main class="max-w-screen-xl mx-auto px-4 min-h-screen">
+        <div>hhh</div>
+        {{ numbers }}
+        <div class="rounded border">
+            <WordComponent
+                v-for="(item, index) in numbers"
+                :key="index"
+                :item="item"
+            />
         </div>
     </main>
 </template>
+<script setup lang="ts">
+const numbers: any = [
+    {
+        id: 1,
+        korean: "영",
+        read: "yeong",
+        english: "zero",
+        lao: "ສູນ",
+        speech: "n",
+        examples: [
+            {
+                id: 1,
+                korean: "숫자 영",
+                read: "sutjja yeong",
+                english: "number zero",
+                lao: "ເລກສູນ",
+            },
+        ],
+    },
+    {
+        id: 2,
+        korean: "일",
+        read: "il",
+        english: "one",
+        lao: "ຫນຶງ",
+        speech: "n",
+        examples: [
+            {
+                id: 1,
+                korean: "자정이 되기 일 분 전입니다.",
+                read: "Jajeongi doegi il bun jeonimnida.",
+                english: "It's one minute to midnight.",
+                lao: "ເລກສູນ",
+            },
+        ],
+    },
+    {
+        id: 3,
+        korean: "이",
+        read: "i",
+        english: "two",
+        lao: "ສອງ",
+        speech: "n",
+        examples: [
+            {
+                id: 1,
+                korean: "표 2장",
+                read: "pyo 2jang",
+                english: "two tickets",
+                lao: "ປີ້ສອງອັນ",
+            },
+        ],
+    },
+];
+</script>
