9712d7e5943a720e676300935e8bb81b789ef318
Soulin Maikadua <sou.dev3@gmail.com>
2024-06-04 18:12:31 +0700

    office commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,22 @@
+import { defineNuxtConfig } from "nuxt";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	app: {
-		head: {
-			title: 'BeYou Studio'
-		}
-	},
-	devtools: { enabled: true }
-})
+  app: {
+    head: {
+      htmlAttris: {
+        en: "en",
+      },
+      charset: "utf-16",
+      title: "BeYou Studio",
+      viewport: "width=device-width, initial-scale=1",
+	  meta:[
+		{
+			name: 'google-site-verification',
+			content: 'eGaVqsClWH0Kk8xrXiyI6ImZEbIwSfpnpW-TTA3gz_U'
+		},
+	  ]
+    },
+    // google-site-verification=eGaVqsClWH0Kk8xrXiyI6ImZEbIwSfpnpW-TTA3gz_U
+  },
+  devtools: { enabled: true },
+});
