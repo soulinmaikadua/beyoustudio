bf0b06d9414e715e32ca519af9db9a59e270d2ff
Soulin Maikadua <sou.dev3@gmail.com>
2024-06-13 19:53:08 +0700

    home commit

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,9 +1,9 @@
 <template>
     <main class="max-w-screen-xl mx-auto px-4 min-h-screen">
-        <div class="mt-12 md:mb-60">
+        <div>
             <div class="grid grid-cols-1">
                 <div class="col-span-2">
-                    <h1 class="text-2xl font-bold border-b-2 pb-5">
+                    <h1 class="text-2xl font-bold border-b-2 py-5 md:py-32">
                         Contact BeYou Studio
                     </h1>
 
@@ -19,16 +19,6 @@
                        Follow or contact us through social  
                     </h3>
                    
-
-                    <button
-                        class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
-                    >
-                        <span
-                            class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
-                        >
-                            Facbook
-                        </span>
-                    </button>
                 </div>
             </div>
         </div>
