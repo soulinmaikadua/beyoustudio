01f730e5ec3493a9b0bf51531bf0fcb7f87f9588
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2024-09-03 11:29:45 +0700

    office commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,3 +8,46 @@
         <Footer />
     </div>
 </template>
+<script setup lang="ts">
+const route = useRoute();
+
+useHead({
+    title: SITE_TITLE + " - A learning platform",
+    meta: [
+        // Open Graph / Facebook
+        {
+            property: "og:title",
+            content: SITE_TITLE + " - A learning platform",
+        },
+        { property: "og:url", content: "https://beyoustudio.pro" + route.path },
+        {
+            property: "og:image",
+            content: "https://beyoustudio.pro/logo.png",
+        },
+        //  Twitter
+        {
+            property: "twitter:title",
+            content: SITE_TITLE + " - A learning platform",
+        },
+        { property: "twitter:card", content: "summary_large_image" },
+        {
+            property: "twitter:url",
+            content: "https://beyoustudio.pro" + route.path,
+        },
+        {
+            property: "twitter:image",
+            content: "https://beyoustudio.pro/logo.png",
+        },
+    ],
+    link: [{ rel: "canonical", href: "https://beyoustudio.pro" + route.path }],
+});
+useSeoMeta({
+    ogType: "website",
+    ogImage: () => "https://beyoustudio.pro/logo.png",
+    ogImageAlt: () => "BeYou Studio Logo",
+    ogSiteName: "BeYou Studio",
+    ogImageWidth: () => 300,
+    ogImageHeight: () => 300,
+    ogImageType: () => "image/png",
+});
+</script>
